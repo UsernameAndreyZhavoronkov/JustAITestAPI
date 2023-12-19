d5583f51debeaa271e973a030522d239e8f6bcc3
UsernameAndreyZhavoronkov <88831906+UsernameAndreyZhavoronkov@users.noreply.github.com>
2023-12-19 13:43:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def redirect_to_https(request: Request, call_next):
 
 @app.get("/")
 def get_hello():
-    return {"message": "add"}
+    return {"color": "green"}
 
 
 def send_get_request(api_url):
